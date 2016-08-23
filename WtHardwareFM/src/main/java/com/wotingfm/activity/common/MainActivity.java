c48700e87ef18cd5c424a3cfaeb4535de73a76ba
xl-xinlong <645700751@qq.com>
2016-08-23 23:03:24 +0800

    添加硬件代码前端编码必要文件

--- WtHardwareFM/src/main/java/com/wotingfm/activity/common/MainActivity.java
+++ WtHardwareFM/src/main/java/com/wotingfm/activity/common/MainActivity.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.wotingfm.R;
-
+/**
+ * 主页
+ * 作者：xinlong on 2016/8/23 22:59
+ * 邮箱：645700751@qq.com
+ */
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
     }
 }
--- WtHardwareFM/src/main/java/com/wotingfm/common/application/BSApplication.java
+++ WtHardwareFM/src/main/java/com/wotingfm/common/application/BSApplication.java
@@ -1,13 +1,28 @@
 package com.wotingfm.common.application;
 
 import android.app.Application;
+import android.content.Context;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.RequestQueue;
 
-/**
- * BSApplication
- * @author 辛龙
- *2016年3月7日
- */
 public class BSApplication extends Application {
+    private static Context instance;
+    private static RequestQueue queues;
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance=this;
+        queues = Volley.newRequestQueue(this);
+
+    }
+
+    public static Context getAppContext(){
+        return instance;
+    }
+
+    public static RequestQueue getHttpQueues() {
+        return queues;
+    }
 
 }
