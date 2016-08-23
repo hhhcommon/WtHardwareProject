2b41d33350f261df9c2fe3daa57f8039ef87a9fb
xl-xinlong <645700751@qq.com>
2016-08-23 18:00:00 +0800

    硬件部分原始代码提交

---
+++ WtHardwareFM/src/main/java/com/wotingfm/common/application/BSApplication.java
@@ -0,0 +1,13 @@
+package com.wotingfm.common.application;
+
+import android.app.Application;
+
+/**
+ * BSApplication
+ * @author 辛龙
+ *2016年3月7日
+ */
+public class BSApplication extends Application {
+
+
+}
