e876ff44caac99a3d4f7d8657e59435e85b2c0a6
54PirateGit <18179739967@163.com>
2016-08-27 18:16:16 +0800

    commit  消息样式更改  添加BaseActivity  08/27  _1

--- WtHardwareFM/src/main/java/com/wotingfm/activity/common/interphone/creategroup/main/CreateGroupMainActivity.java
+++ WtHardwareFM/src/main/java/com/wotingfm/activity/common/interphone/creategroup/main/CreateGroupMainActivity.java
@@ -1,16 +1,12 @@
 package com.wotingfm.activity.common.interphone.creategroup.main;
 
-import android.annotation.TargetApi;
-import android.app.Activity;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
-import android.widget.TextView;
 
 import com.wotingfm.R;
 import com.wotingfm.activity.common.interphone.creategroup.create.CreateGroupItemActivity;
+import com.wotingfm.common.base.BaseActivity;
 import com.wotingfm.common.constant.IntegerConstant;
 import com.wotingfm.common.constant.StringConstant;
 
@@ -18,46 +14,35 @@ import com.wotingfm.common.constant.StringConstant;
  * 创建群组页面
  * Created by Administrator on 2016/8/24 0024.
  */
-public class CreateGroupMainActivity extends Activity implements View.OnClickListener {
+public class CreateGroupMainActivity extends BaseActivity implements View.OnClickListener {
 
-    @TargetApi(Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_group_main);
-
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);		// 透明状态栏
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);	// 透明导航栏
-
-        initViews();
     }
 
-    /*
-     * 初始化视图
-     */
-    private void initViews(){
-        TextView textTitle = (TextView) findViewById(R.id.text_title);
-        textTitle.setText("创建群组");
+    @Override
+    protected int setViewId() {
+        return R.layout.activity_create_group_main;
+    }
 
-        TextView leftBack = (TextView) findViewById(R.id.left_back);
-        leftBack.setOnClickListener(this);
+    @Override
+    protected void init() {
+        setTitle("创建群组");
 
-        View relativeCreatePublic = findViewById(R.id.relative_create_public);               // 创建公开群 无需验证可加入
+        View relativeCreatePublic = findView(R.id.relative_create_public);               // 创建公开群 无需验证可加入
         relativeCreatePublic.setOnClickListener(this);
 
-        View relativeCreatePrivate = findViewById(R.id.relative_create_private);             // 创建密码群 需要密码才来加入
+        View relativeCreatePrivate = findView(R.id.relative_create_private);             // 创建密码群 需要密码才来加入
         relativeCreatePrivate.setOnClickListener(this);
 
-        View relativeCreateVerification = findViewById(R.id.relative_create_verification);   // 创建验证群 需要通过验证才能加入
+        View relativeCreateVerification = findView(R.id.relative_create_verification);   // 创建验证群 需要通过验证才能加入
         relativeCreateVerification.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.left_back:                    // 返回到上一个页面
-                finish();
-                break;
             case R.id.relative_create_public:       // 创建公开群
                 Intent intentPublic = new Intent(CreateGroupMainActivity.this, CreateGroupItemActivity.class);
                 intentPublic.putExtra(StringConstant.CREATE_GROUP_TYPE, IntegerConstant.CREATE_GROUP_PUBLIC);
@@ -74,11 +59,5 @@ public class CreateGroupMainActivity extends Activity implements View.OnClickLis
                 startActivity(intentVerification);
                 break;
         }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        setContentView(R.layout.activity_null_view);
     }
 }
