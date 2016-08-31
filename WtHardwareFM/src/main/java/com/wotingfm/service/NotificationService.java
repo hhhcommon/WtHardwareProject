31dcb33f738faa4356b92b48254b7591947d0eb9
54PirateGit <18179739967@163.com>
2016-08-31 16:52:04 +0800

    commit  08/31  _3

--- WtHardwareFM/src/main/java/com/wotingfm/service/NotificationService.java
+++ WtHardwareFM/src/main/java/com/wotingfm/service/NotificationService.java
@@ -1,5 +1,6 @@
 package com.wotingfm.service;
 
+import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -10,8 +11,10 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.media.AudioManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -655,6 +658,30 @@ public  class NotificationService   extends  Service{
 		nm.notify(0, notification);
 	}
 
+    /**
+     * 发送通知设置
+     * @param mContext
+     * @param message
+     * @param title
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private void setNewMessageNotify(Context mContext, String message, String title){
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Intent pushIntent = new Intent("pushnnn");
+//        Intent pushIntent = new Intent(mContext, SetActivity.class);
+//        PendingIntent in = PendingIntent.getActivity(mContext, 0, pushIntent, 0);
+        PendingIntent in = PendingIntent.getBroadcast(mContext, 2, pushIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(mContext);
+        mBuilder.setContentTitle(title)// 设置通知栏标题
+                .setContentText(message)// 设置通知栏显示内容
+                .setContentIntent(in)// 设置通知栏点击意图
+                .setWhen(System.currentTimeMillis())// 通知产生时间
+                .setPriority(Notification.PRIORITY_DEFAULT)// 设置该通知优先级
+                .setAutoCancel(true)// 设置点击通知消息时通知栏的通知自动消失
+                .setDefaults(Notification.DEFAULT_VIBRATE)// 通知声音、闪灯和振动方式为使用当前的用户默认设置
+                .setSmallIcon(R.mipmap.app_logo);// 设置通知图标
+        mNotificationManager.notify(0, mBuilder.build());
+    }
 
 //	Notification notification = new Notification.Builder(context)
 //	//设置通知标题
