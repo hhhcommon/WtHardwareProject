523ef1ae2e53636c0af6e8f7ba0096b9171fc412
xl-xinlong <645700751@qq.com>
2016-10-11 18:47:18 +0800

    添加wifiactivity

--- WtHardwareFM/src/main/java/com/wotingfm/activity/common/welcome/fragment/WelcomebFragment.java
+++ WtHardwareFM/src/main/java/com/wotingfm/activity/common/welcome/fragment/WelcomebFragment.java
@@ -14,29 +14,27 @@ import com.wotingfm.util.BitmapUtils;
 
 /**
  * 第二张引导页
- * @author 辛龙
- * 2016年4月27日
+ * 作者：xinlong on 2016/4/27 21:18
+ * 邮箱：645700751@qq.com
  */
-public class WelcomebFragment extends Fragment  {
-	private FragmentActivity context;
-	private ImageView imageView1;
-	private Bitmap bmp;
+public class WelcomeBFragment extends Fragment {
+    private Bitmap bmp;
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		FragmentActivity context = this.getActivity();
-		View rootView = inflater.inflate(R.layout.item_welcomeb, container, false);
-		ImageView imageView1 = (ImageView)rootView.findViewById(R.id.imageView1);
-		bmp = BitmapUtils.readBitMap(context, R.mipmap.welcomeb);
-		imageView1.setImageBitmap(bmp);
-		return rootView;
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        FragmentActivity context = this.getActivity();
+        View rootView = inflater.inflate(R.layout.item_welcomeb, container, false);
+        ImageView imageView1 = (ImageView) rootView.findViewById(R.id.imageView1);
+        bmp = BitmapUtils.readBitMap(context, R.mipmap.welcomeb);
+        imageView1.setImageBitmap(bmp);
+        return rootView;
+    }
 
-	@Override
-	public void onDestroy() {
-		super.onDestroy();
-		if(bmp != null && !bmp.isRecycled()) {  
-			bmp.recycle();  
-		} 
-	}
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (bmp != null && !bmp.isRecycled()) {
+            bmp.recycle();
+        }
+    }
 }
