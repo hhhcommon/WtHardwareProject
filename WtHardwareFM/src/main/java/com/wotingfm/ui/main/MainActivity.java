7a9c5b7ddf064bb1739bafec5a4df2d7ffe19b6c
54PirateGit <18179739967@163.com>
2017-03-24 17:41:07 +0800

    commit  03/24    _1

--- WtHardwareFM/src/main/java/com/wotingfm/ui/main/MainActivity.java
+++ WtHardwareFM/src/main/java/com/wotingfm/ui/main/MainActivity.java
@@ -43,7 +43,6 @@ import com.wotingfm.common.receiver.NetWorkChangeReceiver;
 import com.wotingfm.common.service.FloatingWindowService;
 import com.wotingfm.common.service.LocationService;
 import com.wotingfm.common.service.NotificationService;
-import com.wotingfm.common.service.SimulationService;
 import com.wotingfm.common.service.SocketService;
 import com.wotingfm.common.service.SubclassService;
 import com.wotingfm.common.service.TestWindowService;
@@ -170,8 +169,8 @@ public class MainActivity extends TabActivity {
         startService(Notification);
         FloatingWindow = new Intent(this, FloatingWindowService.class);//启动全局弹出框服务
         startService(FloatingWindow);
-        Simulation=new Intent(this,SimulationService.class);
-        startService(Simulation);
+//        Simulation=new Intent(this,SimulationService.class);
+//        startService(Simulation);
         TestFloatingWindow = new Intent(this, TestWindowService.class);//启动全局弹出框服务
         startService(TestFloatingWindow);
     }
